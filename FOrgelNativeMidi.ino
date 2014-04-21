30eeec44cf8c8b096bf6bd4596f96ec0c583a609
surma-lodur <sotex@gmx.de>
2014-04-21 18:25:44 +0200

    added detune test, needs to be fixed

--- FOrgelNativeMidi.ino
+++ FOrgelNativeMidi.ino
@@ -7,26 +7,36 @@
 void setup() {
   MIDI.begin();           // Launch MIDI, by default listening to channel 1.
   Harmonic::calculate(0);
-  PulsR::add(new  PulsR::Neutron(2));
-  PulsR::add(new  PulsR::Neutron(3));
+  //PulsR::add(new  PulsR::Neutron(4,5, FLOPPY_SECTORS));
+  //PulsR::add(new  PulsR::Neutron(2,3, FLOPPY_SECTORS));
+
+  PulsR::add(new  PulsR::Neutron(5));
   PulsR::add(new  PulsR::Neutron(4));
+  PulsR::add(new  PulsR::Neutron(3));
+  PulsR::add(new  PulsR::Neutron(2));
 }
 
 
 void loop() {
   if (MIDI.read()) {
     switch (MIDI.getType()) {
-      case midi::NoteOn:       
+      case midi::NoteOn:
         PulsR::pulses(
           noteToMicros(MIDI.getData1())
         );
         break;
-      case midi::NoteOff:      
+      case midi::NoteOff:
         PulsR::collapse(
           noteToMicros(MIDI.getData1())
         );
         break;
-        // See the online reference for other message types
+      case midi::PitchBend: {
+        int lsb = MIDI.getData1() << 8;
+        int hsb = MIDI.getData2();
+        
+        PulsR::detune(lsb & hsb);
+        break;
+        }
       default:
         break;
     }
